e55692da2306a812206da1229861c8164d1fc22b
SalomonAvila <avila_sa@javeriana.edu.co>
2026-02-14 19:44:21 -0500

    Avances en UI de MainActivity y actualización de dependencias

--- app/src/main/java/com/example/buy_it/MainActivity.kt
+++ app/src/main/java/com/example/buy_it/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.buy_it.ui.screens.editinfo.EditInfo
+import com.example.buy_it.ui.screens.login.Login
 import com.example.buy_it.ui.screens.profile.Profile
+import com.example.buy_it.ui.screens.register.Register
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Profile()
+            Register()
         }
     }
 }
