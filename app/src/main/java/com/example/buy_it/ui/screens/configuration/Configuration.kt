49bcb60e98afdff824255121c368a979d809b40b
SalomonAvila <avila_sa@javeriana.edu.co>
2026-02-19 20:52:43 -0500

    Creación de la pantalla de configuración

--- app/src/main/java/com/example/buy_it/ui/screens/configuration/Configuration.kt
+++ app/src/main/java/com/example/buy_it/ui/screens/configuration/Configuration.kt
@@ -1,11 +1,24 @@
 package com.example.buy_it.ui.screens.configuration
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoGraph
+import androidx.compose.material.icons.filled.Cloud
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.DataThresholding
+import androidx.compose.material.icons.filled.DoubleArrow
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Nightlight
+import androidx.compose.material.icons.filled.NoAccounts
+import androidx.compose.material.icons.filled.NotificationsActive
+import androidx.compose.material.icons.filled.Storage
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -37,10 +50,11 @@ fun Configuration(
             modifier = Modifier
                 .padding(start = 10.dp, top = 20.dp, bottom = 15.dp)
                 .size(30.dp)
+                .clickable(onClick = { onBackPressed() })
         )
 
         Text(
-            text = "Configuraciones",
+            text = stringResource(R.string.configuraciones),
 
             style = TextStyle(
                 fontSize = 45.sp,
@@ -60,7 +74,25 @@ fun Configuration(
                 .padding(start = 15.dp)
         )
         SearchBar()
-        SubsectionText("Quien pude ver tus reseñas", Modifier.padding(start = 20.dp, top = 15.dp))
+        SubsectionText(stringResource(R.string.quien_pude_ver_tus_rese_as), Modifier.padding(start = 20.dp, top = 35.dp))
+        SubsectionItem(Icons.Default.Lock,
+            stringResource(R.string.privacidad_de_la_cuenta), stringResource(R.string.privada)
+        )
+        SubsectionItem(Icons.Default.NoAccounts, stringResource(R.string.bloqueados),
+            stringResource(R.string._11)
+        )
+        SubsectionText(stringResource(R.string.general), Modifier.padding(start = 20.dp, top = 35.dp))
+        SubsectionItem(Icons.Default.Language, stringResource(R.string.idioma),
+            stringResource(R.string.espa_ol)
+        )
+        SubsectionItem(Icons.Default.NotificationsActive, stringResource(R.string.notificaciones))
+        SubsectionItem(Icons.Default.DarkMode, stringResource(R.string.temas),
+            stringResource(R.string.claro)
+        )
+        SubsectionItem(Icons.Default.DataThresholding, stringResource(R.string.tiempo_en_pantalla))
+        SubsectionItem(Icons.Default.Cloud, stringResource(R.string.uso_de_datos))
+        SubsectionItem(Icons.Default.Storage, stringResource(R.string.almacenamiento))
+
     }
 }
 
--- app/src/main/java/com/example/buy_it/ui/screens/configuration/ConfigurationComponents.kt
+++ app/src/main/java/com/example/buy_it/ui/screens/configuration/ConfigurationComponents.kt
@@ -9,6 +9,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -36,7 +43,7 @@ fun SearchBar(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = 15.dp, vertical = 10.dp)
+            .padding(horizontal = 30.dp, vertical = 5.dp)
             .height(55.dp)
     ) {
         Image(
@@ -82,6 +89,7 @@ fun SubsectionText(
         style = TextStyle(
             fontSize = 14.sp,
             fontWeight = FontWeight(860),
+            color = MaterialTheme.colorScheme.primary
         )
     )
 }
@@ -90,4 +98,57 @@ fun SubsectionText(
 @Preview
 fun SubsectionTextPreview(){
     SubsectionText("Sapo")
+}
+
+@Composable
+fun SubsectionItem(
+    icon: ImageVector,
+    text: String,
+    secondText: String = "",
+    modifier : Modifier = Modifier
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = stringResource(R.string.inicio),
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(30.dp).padding(end = 8.dp)
+        )
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text,
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight(860),
+                color = MaterialTheme.colorScheme.primary
+            ),
+        )
+        Text(
+            modifier = Modifier.padding(end = 8.dp),
+            text = secondText,
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight(860),
+                color = MaterialTheme.colorScheme.primary
+            )
+        )
+        Icon(
+            imageVector = Icons.Default.ArrowBackIosNew,
+            contentDescription = stringResource(R.string.inicio),
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(22.dp).graphicsLayer{scaleX=-1f}
+        )
+    }
+
+}
+
+@Composable
+@Preview
+fun SubsectionItemPreview(){
+    SubsectionItem(Icons.Default.Lock,"Privacidad de la cuenta", "asass", Modifier)
 }
