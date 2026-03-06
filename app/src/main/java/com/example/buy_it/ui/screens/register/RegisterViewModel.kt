9e77d902aa67b4e53a6dfa9048ce2a3f35e89fd2
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-03-05 21:40:50 -0500

    register state mensaje de error

--- app/src/main/java/com/example/buy_it/ui/screens/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/buy_it/ui/screens/register/RegisterViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class RegisterViewModel : ViewModel() {
@@ -13,23 +12,23 @@ class RegisterViewModel : ViewModel() {
     val uiState: StateFlow<RegisterState> = _uiState
 
     fun onUsernameChange(value: String) {
-        _uiState.update { it.copy(username = value) }
+        _uiState.update { it.copy(username = value, errorMessage = "") }
     }
 
     fun onEmailChange(value: String) {
-        _uiState.update { it.copy(email = value) }
+        _uiState.update { it.copy(email = value, errorMessage = "") }
     }
 
     fun onPasswordChange(value: String) {
-        _uiState.update { it.copy(password = value) }
+        _uiState.update { it.copy(password = value, errorMessage = "") }
     }
 
     fun onConfirmPasswordChange(value: String) {
-        _uiState.update { it.copy(confirmPassword = value) }
+        _uiState.update { it.copy(confirmPassword = value, errorMessage = "") }
     }
 
     fun onAcceptedTermsChange() {
-        _uiState.update { it.copy(acceptedTerms = !it.acceptedTerms) }
+        _uiState.update { it.copy(acceptedTerms = !it.acceptedTerms, errorMessage = "") }
     }
 
     fun onToggleMostrarPassword() {
@@ -42,11 +41,21 @@ class RegisterViewModel : ViewModel() {
 
     fun onRegister() {
         val state = _uiState.value
-        Log.d(
-            "Envio de informacion",
-            "username: ${state.username}, email: ${state.email}, " +
-                    "contraseña: ${state.password}, confirmar contraseña: ${state.confirmPassword}, " +
-                    "Recordar contraseña: ${state.acceptedTerms}"
-        )
+        if (
+            state.username.isNullOrEmpty() ||
+            state.email.isNullOrEmpty() ||
+            state.password.isNullOrEmpty() ||
+            state.confirmPassword.isNullOrEmpty() ||
+            !state.acceptedTerms
+        ) {
+            _uiState.update { it.copy(mostrarMensaje = true, errorMessage = "Todos los campos son obligatorios") }
+        } else {
+            Log.d(
+                "Envio de informacion",
+                "username: ${state.username}, email: ${state.email}, " +
+                        "contraseña: ${state.password}, confirmar contraseña: ${state.confirmPassword}, " +
+                        "Recordar contraseña: ${state.acceptedTerms}"
+            )
+        }
     }
 }
