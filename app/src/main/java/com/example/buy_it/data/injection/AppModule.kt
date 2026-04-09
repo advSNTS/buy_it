4c949dd0c6068e2e8ee70187d8d7b9e700b2a42f
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-04-09 14:46:50 -0500

    object

---
+++ app/src/main/java/com/example/buy_it/data/injection/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.buy_it.data.injection
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule{
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:3000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+}
