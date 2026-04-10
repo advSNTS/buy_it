b8ba41072b2bf6b672dbf6dde8d9abffb32cbf05
Miguel Ángel García Rodríguez <miguelagarcia@javeriana.edu.co>
2026-04-09 23:26:20 -0500

    Publicar, editar y eliminar reseña antes del pr

--- app/src/main/java/com/example/buy_it/data/datasource/services/ReviewRetrofitService.kt
+++ app/src/main/java/com/example/buy_it/data/datasource/services/ReviewRetrofitService.kt
@@ -2,13 +2,13 @@ package com.example.buy_it.data.datasource.services
 
 import com.example.buy_it.data.dtos.CreateReviewDTO
 import com.example.buy_it.data.dtos.ReviewDTO
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.Response
 
 interface ReviewRetrofitService {
 
