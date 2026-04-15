553c665d217264a75689f226cab17770b3ff033b
advSNTS <187468015+advSNTS@users.noreply.github.com>
2026-04-15 10:38:07 -0500

    correccion

--- app/src/main/java/com/example/buy_it/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/buy_it/data/repository/UserRepository.kt
@@ -1,13 +1,15 @@
 package com.example.buy_it.data.repository
 
+import android.util.Log
 import com.example.buy_it.data.ReviewInfo
 import com.example.buy_it.data.UserProfileInfo
 import com.example.buy_it.data.datasource.UserRemoteDatasource
+import com.example.buy_it.data.datasource.impl.firestore.UserFirestoreDataSourceImpl
 import com.example.buy_it.data.dtos.RegisterUserDto
 import com.example.buy_it.data.dtos.toReviewInfo
 import javax.inject.Inject
 
-class UserRepository @Inject constructor(private val userRemoteDatasource: UserRemoteDatasource) {
+class UserRepository @Inject constructor(private val userRemoteDatasource: UserFirestoreDataSourceImpl) {
 
     suspend fun getUserById(id: String): Result<UserProfileInfo> {
         return try {
@@ -33,8 +35,10 @@ class UserRepository @Inject constructor(private val userRemoteDatasource: UserR
         return try {
             val registerUserDto = RegisterUserDto(username)
             userRemoteDatasource.registerUser(registerUserDto, userId)
+            Log.d("TAG", "usuario registrado, id: $userId")
             Result.success(Unit)
         } catch (e: Exception) {
+            Log.d("TAG", "error en register repository, id: ${e.message}")
             Result.failure(e)
         }
     }
